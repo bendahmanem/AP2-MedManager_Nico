2d7c6dbd0dd175bc73c72115a6fa558cc02fc487
MeechyFZ <ponsnicolas7@gmail.com>
2024-10-02 17:09:03 +0200

    SecondCommit

---
+++ Models/Medecin.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+public class Medecin
+{
+    public int MedecinId { get; set; }
+    [Required(ErrorMessage = "Le nom est obligatoire")]
+    [StringLength(50, MinimumLength = 2)]
+    public string Nom { get; set; }
+    [StringLength(50, MinimumLength = 2)]
+    [Required(ErrorMessage = "Le prénom est obligatoire")]
+    public string Prenom { get; set; }
+	public string? Ville { get; set; }
+    [StringLength(50, MinimumLength = 2)]
+    [Required(ErrorMessage = "Le nom d'utilisateur est obligatoire")]
+    public string Utilisateur { get; set; }
+    [DataType(Datatype.Password)]
+    [Required(ErrorMessage = "Le mot de passe est obligatoire")]
+    public string MotDePasse { get; set; }	
+    public List<Patient> Patients { get; set; }
+    public List<Ordonnance> Ordonnances { get; set; }
+}
---
+++ Models/Ordonnance.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+public class Medecin
+{
+    public int MedecinId { get; set; }
+
+    [Required(ErrorMessage = "Le nom est obligatoire")]
+    [StringLength(50, MinimumLength = 2)]
+    public string Nom { get; set; }
+
+    [Required(ErrorMessage = "Le prénom est obligatoire")]
+    [StringLength(50, MinimumLength = 2)]
+    public string Prenom { get; set; }
+
+    public string? Ville { get; set; }
+
+    [Required(ErrorMessage = "Le nom d'utilisateur est obligatoire")]
+    [StringLength(50, MinimumLength = 2)]
+    public string Utilisateur { get; set; }
+
+    [DataType(DataType.Password)]
+    [Required(ErrorMessage = "Le mot de passe est obligatoire")]
+    public string MotDePasse { get; set; }
+
+    public List<Patient> Patients { get; set; } = new List<Patient>();
+
+    public List<Ordonnance> Ordonnances { get; set; } = new List<Ordonnance>();
+}
