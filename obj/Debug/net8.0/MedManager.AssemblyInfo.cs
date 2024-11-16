f85897d748126ea4af8b361c5468d75054231a69
MeechyFZ <ponsnicolas7@gmail.com>
2024-11-16 16:50:23 +0100

    WIP toast

--- obj/Debug/net8.0/MedManager.AssemblyInfo.cs
+++ obj/Debug/net8.0/MedManager.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MedManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f92f5ba3faa92ca2c85086be03c6a0f7ae808226")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d42652bd65c580b03b88b36c0dd8e9bb873d14ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("MedManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MedManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
