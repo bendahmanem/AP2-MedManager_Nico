74ff8cfc9b0d18771c65361cced1e7a443c97701
MeechyFZ <ponsnicolas7@gmail.com>
2024-11-21 11:50:58 +0100

    Médicament messages si données absentes : OK

--- obj/Debug/net8.0/MedManager.AssemblyInfo.cs
+++ obj/Debug/net8.0/MedManager.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MedManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+618b79c7dd110fbf9adc1172f00a99d4948ad91a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73dcb3ff12381c4349fdae63b25387daa14ba2f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MedManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MedManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
