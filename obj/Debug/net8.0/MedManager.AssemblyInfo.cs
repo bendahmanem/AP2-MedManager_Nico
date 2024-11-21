669d5a3ce0010ea66344a91b5b4515ef8c06bfae
MeechyFZ <ponsnicolas7@gmail.com>
2024-11-21 21:40:13 +0100

    Message popover + Message erreur si recherche HS

--- obj/Debug/net8.0/MedManager.AssemblyInfo.cs
+++ obj/Debug/net8.0/MedManager.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MedManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75b1aa97b9e7c46462d5fff55b0de8ea5f90b68f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5af30eca4fd4aecdfa9b0acb208cc65808a13776")]
 [assembly: System.Reflection.AssemblyProductAttribute("MedManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MedManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
