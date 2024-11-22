d394a1b5694e14400ec45cabd75d5c4eb352ee46
MeechyFZ <ponsnicolas7@gmail.com>
2024-11-22 13:11:35 +0100

    Déplacer toasts dans layout: OK

--- obj/Debug/net8.0/MedManager.AssemblyInfo.cs
+++ obj/Debug/net8.0/MedManager.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MedManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b38e9da9d689a890199500ae75a0fca536e1dd5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38a615e604382d41ddad44068671ff60a010e4b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MedManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MedManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
